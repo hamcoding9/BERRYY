938be5ef8aaf7f6ca5fae31d9cf5e7e869fff517
hamcoding9 <dientes8520@gmail.com>
2023-03-02 14:25:07 +0900

    [feat] onboarding fragment 접속 시 nav bar 안보임 처리

--- app/src/main/java/com/hamcoding/berryy/MainActivity.kt
+++ app/src/main/java/com/hamcoding/berryy/MainActivity.kt
@@ -2,12 +2,15 @@ package com.hamcoding.berryy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hamcoding.berryy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,13 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.onBoardingFragment -> navView.visibility = View.GONE
+                else -> navView.visibility = View.VISIBLE
+            }
+        }
+
         binding.bottomNavigation.setupWithNavController(navController)
     }
 }

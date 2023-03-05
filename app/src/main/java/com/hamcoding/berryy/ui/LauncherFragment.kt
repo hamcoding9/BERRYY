3aead903b245d840f0f825747607fee3abc0a297
hamcoding9 <dientes8520@gmail.com>
2023-03-05 20:59:51 +0900

    [feat] 앱 진입점 수정

--- app/src/main/java/com/hamcoding/berryy/ui/LauncherFragment.kt
+++ app/src/main/java/com/hamcoding/berryy/ui/LauncherFragment.kt
@@ -7,29 +7,42 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.hamcoding.berryy.R
+import com.hamcoding.berryy.databinding.FragmentLauncherBinding
 
 class LauncherFragment : Fragment() {
+
+    private var _binding: FragmentLauncherBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_launcher, container, false)
+    ): View {
+        _binding = FragmentLauncherBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val isOnBoarding = true
-        if (isOnBoarding) {
+//        val isOnBoarding = true
+//        if (isOnBoarding) {
+//            findNavController().navigate(R.id.action_launcherFragment_to_onBoardingFragment)
+//        } else {
+//            findNavController().navigate(R.id.action_launcherFragment_to_navigation_home)
+//        }
+        binding.btnOnboarding.setOnClickListener {
             findNavController().navigate(R.id.action_launcherFragment_to_onBoardingFragment)
-        } else {
-            findNavController().navigate(R.id.action_launcherFragment_to_navigation_home)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
